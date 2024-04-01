69b93c7ade3b8d0cf9e0600eb61bf5704a092063
Kauzue <kauzue@naver.com>
2024-04-02 01:23:03 +0900

    귀신의 아이 개발 시작

---
+++ Kinoko/Init.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define NUM_MAX_SAVES 20
+#define NUM_MAX_SAVE_PER_ITEMS 5
+
+typedef struct item {
+	int number;
+} item_t;
+
+typedef struct save {
+	int item_num;
+	item_t items[NUM_MAX_SAVE_PER_ITEMS];
+	int stage;
+} save_t;
+
+item_t s_items[NUM_MAX_SAVE_PER_ITEMS];
+save_t s_saves[NUM_MAX_SAVES];
+
+void Init();
---
+++ Kinoko/main.c
@@ -0,0 +1,10 @@
+#include "Init.h"
+#include "game_main.h"
+
+int main(void)
+{
+	Init();
+	screen();
+
+	return 0;
+}
